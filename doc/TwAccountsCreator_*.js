3baabc2c7ee97d36c266bd30c6e332870571e1d7
Jinzo <36779490+jinzocode@users.noreply.github.com>
2018-02-27 22:11:09 +0200

    Create TwAccountsCreator_*.js

---
+++ doc/TwAccountsCreator_*.js
@@ -0,0 +1,42 @@
+![TWITTER-1.jpg](https://res.cloudinary.com/hpiynhbhq/image/upload/v1519434336/cfk3nmv42yfrf8n6qo0v.jpg)
+
+# Twitter Account Creator - Moakt
+
+### What is the project about?
+This is a new project for a macro that is creating twitter accounts by using temp email service which is moakt.com. The script contains many interactive functions that keep pace with most site changes.
+
+The script has many features:
+
+- Auto Upload Image
+- Random Full Name Generator
+- Random Email Generator
+- Random Username Generator
+- Auto Confirm Email Address
+- Automatic avoidance of errors
+- Counter
+
+### How to use?
+
+Download
+
+- Firefox ESR
+- [iMacros 8.9.7](https://addons.mozilla.org/firefox/downloads/file/428670/imacros_for_firefox-8.9.7-fx.xpi)
+
+Execute the command:
+
+``` git clone https://github.com/jinzocode/imacros-scripts.git```
+
+Then Add ```(TwAccountsCreator_Moakt.js)```
+
+to your ```iMacros/Macros``` Folder
+
+Then open firefox and hit ```F8```
+
+That's it
+### Roadmap
+
+- Continuous maintenance
+- Any Improvements needed
+
+### How to contribute?
+Open a new pull request or just leave your feedback or a comment below.
