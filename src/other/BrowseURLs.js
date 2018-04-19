f61f27ba71050d37464464b615922c21ea27ddd3
Jinzo <36779490+jinzocode@users.noreply.github.com>
2018-04-19 05:13:52 +0200

    :new: Auto Browse URLs from Datasource (browseurls.txt)

---
+++ src/other/BrowseURLs.js
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////
+//@github https://github.com/jinzocode/imacros-scripts//
+///////////////////////////////////////////////////////
+var jsLF = "\n";
+var i, retcode, errtext, macro;
+var windowMediator = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+    .getService(Components.interfaces.nsIWindowMediator);
+var window = windowMediator.getMostRecentWindow("navigator:browser");
+var iMacros = window.QueryInterface(imns.Ci.nsIInterfaceRequestor)
+    .getInterface(imns.Ci.nsIWebNavigation)
+    .QueryInterface(imns.Ci.nsIDocShellTreeItem).rootTreeItem
+    .QueryInterface(imns.Ci.nsIInterfaceRequestor)
+    .getInterface(imns.Ci.nsIDOMWindow).iMacros;
+var filename = iMacros._currentMacro.name;
+var datapath = (iMacros._currentMacro.path).replace(filename, '').replace(/\\Macros\\/g, '\\Datasources\\');
+
+var count = 0;
+var txtfile = "browseurls.txt";
+var urls = lineCount(datapath + txtfile);
+///////////////////////////////////////////////////
+var min = +prompt("Interval in seconds:");
+if (min) {
+    for (i = 1; i < lineCount(datapath + txtfile); i++) {
+        var forums = "CODE:";
+        forums += "SET !ERRORIGNORE YES" + jsLF;
+        forums += "SET !TIMEOUT_PAGE 39" + jsLF;
+        forums += "SET !DATASOURCE " + txtfile + jsLF;
+        forums += "SET !DATASOURCE_LINE " + i + jsLF;
+        forums += "FILTER TYPE=IMAGES STATUS=ON" + jsLF;
+        forums += "ONDIALOG POS=1 BUTTON=OK CONTENT=" + jsLF;
+        forums += "URL GOTO={{!COL1}}" + jsLF;
+        forums += "FILTER TYPE=IMAGES STATUS=OFF" + jsLF;
+        iimPlay(forums);
+
+        iimDisplay("Interval: " + min + "\nUrl: " + i);
+        iimPlayCode("SET !ERRORIGNORE YES\nWAIT SECONDS=" + min);
+    }
+    alert('Finished!');
+}
