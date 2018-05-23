cb7affc30e70c442a7dc6c8a8a1b08cadbc0cadb
Jinzo <36779490+jinzocode@users.noreply.github.com>
2018-05-23 13:57:07 +0200

    :new: Gmail Random Accounts Creator

---
+++ src/google/GmailAccountsCreator.js
@@ -0,0 +1,82 @@
+﻿/////////////////////////////////////////////////////////
+//@github https://github.com/jinzocode/imacros-scripts//
+///////////////////////////////////////////////////////
+//A variable being used as memory to remmeber the next loop session by using new lines between every command.
+var jsLF = "\n";
+//Loop, error handling variables
+var i, retcode, errtext;
+//Enumerating all windows of a given type and getting the most recent / any window of a given type.
+var windowMediator = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+    .getService(Components.interfaces.nsIWindowMediator);
+var window = windowMediator.getMostRecentWindow("navigator:browser");
+////////////////////////////////////////////////////////////////////////////////////////
+//iMacros variable to access query interface
+var iMacros = window.QueryInterface(imns.Ci.nsIInterfaceRequestor)
+    .getInterface(imns.Ci.nsIWebNavigation)
+    .QueryInterface(imns.Ci.nsIDocShellTreeItem).rootTreeItem
+    .QueryInterface(imns.Ci.nsIInterfaceRequestor)
+    .getInterface(imns.Ci.nsIDOMWindow).iMacros;
+
+//Create random username for gmail
+function usrname(len) {
+    let text = '';
+    let charset = "abcdefghijklmnopqrstuvwxyz1234567890"; //letters and numbers
+    //Randomize laters
+    for (let i = 0; i < len; i++)
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+    //return username
+    return text;
+}
+
+let trouble = "yourphonenumber"; // for security check
+// let randomnumber = "213"+Math.random().toString().slice(2,7);
+//Browser useragent
+let usragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
+//Names
+let EN_names = ["Awaad", "Abdallah", "Adam", "Ahmad", "Adham", "Acef", "Ashraf", "Harsh", "Aktham", "Akram", "Amjad", "Prince", "Anwar", "Aws", "Awab", "Aimen", "Anas", "Osama", "Islam", "Ikram", "Elhamy", "Eyad", "Eyas", "Faith", "Ehab", "Bashir", "Enmity", "Hassan", "Hussein", "Mansour", "Mu'taz Bellah", "Newborn", "Basil", "Basem", "Wow", "Badir", "Bahaa", "Batal", "Badr", "Badran", "Badr", "pond", "Bassam", "Bashar", "Bakr", "Bashir", "Belal", "Tamim", "Tim", "pious", "Turki", "Thamer", "Jaber", "Earnest", "Jasser", "Jassem", "Jabr", "Gabriel", "Jassar", "Jafar", "Jalal", "great", "Beauty", "Friday", "Jihad", "Hamza", "Hammad", "Hamoud", "Hudhayfah", "Husam", "Hosny", "Hussain", "Hikmet", "Haider", "Khalfan", "Khlefan", "Khalil", "Khuzam", "Dali", "Danny", "Drgham", "Darwish", "Duraid", "Raafat", "Raouf", "Pioneer", "Raef", "Rajeh", "Raji", "Rashed", "Satisfied", "willing", "Rakan", "Ramez", "Rami", "Rashad", "Rushd", "Rushdie", "Rashid", "Ramzi", "Riad", "Chubby", "Zaher", "Zahi", "Zayed", "Zubair", "Zuhair", "Zeyad", "Xian", "Zaid", "Zain", "I will see", "Salem", "Sameh", "Samer", "Sami", "Saher", "nebula", "Saad", "Saud", "Happy", "Sufian", "Sultan", "Salman", "Sulaiman", "Samih", "Samir", "Shady", "Shaker", "Shaheen", "Shaya", "Shaddad", "Sherif", "Shafiq", "Sabir", "Safwat", "Salah", "Dahi", "Dia", "Tareq", "Tayel", "Talal", "Talaat", "Taha", "Zafer", "Abed", "Adel", "Arif", "Amer", "obaida", "Othman", "Adnan", "Arafat", "Almighty", "Azzam", "Izzat", "Azmi", "Aziz", "Essam", "Alaa", "Top", "Emad", "Ammar", "Omar", "Amr", "Amir", "Awni", "Ayyash", "Oday", "Ghaly", "Ghanem", "a stranger", "Ghassan", "Ghoneim", "Ghaith", "Fouad", "Fadi", "Knight", "Farouk", "Faiz", "Fathi", "Pride", "Fida", "Firas", "Unique", "Fazza", "Fahd", "Fahmy", "Fawaz", "Fawzi", "Fayyad", "Faisal", "Qaboos", "Qusay", "Karem", "Kazem", "Canaan", "Loai", "Labib", "Laith", "safe", "Abida", "Adiba", "Afaf", "Afifa", "Ahlem", "Aïcha", "Aïda", "Alia", "Amana", "Amel", "Amina", "Amira", "Anissa", "Asmaa", "Assia", "Atika", "Aya", "Aziza", "Badra", "Basma", "Chadia", "Chafia", "Chafika", "Chahra", "Chahrazad", "Chakira", "Dalila", "Djamila", "Douha", "Dounia", "Emna", "Fadila", "Faiza", "Farida", "Faten", "Fatiha", "Fatima", "Fouzia", "Ghalia", "Ghania", "Habiba", "Hadia", "Hafida", "Hafsa", "Hakima", "Hauled", "Halima", "Hamida", "Hanane", "Hania", "Hanna", "Hayet", "Hawa", "Ibtissem", "Ikram", "Ilhem", "Imane", "Ines", "Karima", "Kawtar", "Kenza", "Khadidja", "Khalida", "Latifa", "Leila", "Mabrouka", "Maha", "Mahbouba", "Maissa", "Majda", "Malika", "Manel", "Meriem", "Moufida", "Mouna", "Mounira", "Nabila", "Nacira", "Nadia", "Nadjiba", "Nafissa", "Naïma", "Najet", "Nawel", "Naziha", "Nedjma", "Nour", "Rachida", "Rahma", "Rajah", "Rawda", "Safia", "Saïda", "Sakina", "Saliha", "Salima", "Saloua", "Samia", "Samira", "Sanaa", "Selma", "Sihème", "Soraya", "Souad", "Wafa", "Wahiba", "Warda", "Wasilla", "Wided", "Yamina", "Yasmine", "Yousra", "Zineb", "Zohra", "Adam", "Adel", "Adib", "Adil", "Ahmed", "Aissa", "Ali", "Amine", "Amir", "Anas", "Anise", "Anouar", "Ayoub", "Azziz", "Bachir", "Badr", "Baligh", "Billal", "Bouzid", "Chafik", "Chahid", "Chaker", "Chams", "Chawki", "Chokri", "Daoud", "Djamel", "Djillali", "Fadel", "Fahim", "Fares", "Farid", "Fathi", "Faudel", "Faouzi", "Fayçal", "Ferhat", "Fouad", "Gibril", "Ghani", "Habib", "Hafid", "Hakim", "Halim", "Hamid", "Hani", "Hichem", "Hilal", "Hosni", "Ismail", "Imed", "Jabar", "Kader", "Kamel", "Karim", "Khaled", "Khalil", "Larbi", "Lotfi", "Mabrouk", "Mahboub", "Mahfoud", "Mahmoud", "Malik", "Marwan", "Mehdi", "Mohamed", "Moktar", "Moncef", "Mourad", "Nabil", "Nacer", "Nadir", "Nadjib", "Nassim", "Nazim", "Nouh", "Omar", "Othmane", "Usama", "Rabah", "Mohamed", "Rachid", "Rafik", "Rahal", "Ramzi", "Raouf", "Rayan", "Réda", "Redouane", "Riad", "Rochdi", "Sabri", "Salah", "Salim", "Sami", "Sedik", "Sofiane", "Tahar", "Taimim", "Tarek", "Tayeb", "Tawfik", "Wallid", "Wassim", "Yacoub", "Yazid", "Youcef", "Younes", "Zahid", "Zaïm", "Zakaria", "Zaki", "Ziad", "Zoheir", "Khalifa", "Reda", "Mazen", "Salah", "Samy", "Rami", "Hossam", "Diab", "Kamel", "Hatem", "Hakem", "Basha", "Zedan", "Nader", "Mina", "Ashor", "Amr", "Abdallah", "Abdulhakem", "Abdulhamed", "Abida", "Adiba", "Afaf", "Afifa", "Ahlem", "Aïcha", "Aïda", "Alia", "Amana", "Amel", "Amina", "Amira", "Anissa", "Asma", "Assia", "Atika", "Aya", "Aziza", "Badra", "Basma", "Chadia", "Chafia", "Chafika", "Chahra", "Chahrazad", "Chakira", "Dalila", "Djamila", "Douha", "Dounia", "Emna", "Fadila", "Faiza", "Farida", "Faten", "Fatiha", "Fatima", "Fouzia", "Ghalia", "Ghania", "Habiba", "Hadia", "Hafida", "Hafsa", "Hakima", "Hauled", "Halima", "Hamida", "Hanane", "Hania", "Hanna", "Hayet", "Hawa", "Ibtissem", "Ikram", "Ilhem", "Imane", "Ines", "Karima", "Kawtar", "Kenza", "Khadidja", "Khalida", "Latifa", "Leila", "Mabrouka", "Maha", "Mahbouba", "Maissa", "Majda", "Malika", "Manel", "Meriem", "Moufida", "Mouna", "Mounira", "Nabila", "Nacira", "Nadia", "Nadjiba", "Nafissa", "Naïma", "Najet", "Nawel", "Naziha", "Nedjma", "Nour", "Rachida", "Rahma", "Rajah", "Rawda", "Safia", "Saïda", "Sakina", "Saliha", "Salima", "Saloua", "Samia", "Samira", "Sanaa", "Selma", "Sihème", "Soraya", "Souad", "Wafa", "Wahiba", "Warda", "Wasilla", "Wided", "Yamina", "Yasmine", "Yousra", "Zineb", "Zohra", "Adam", "Adel", "Adib", "Adil", "Ahmed", "Aissa", "Ali", "Amine", "Amir", "Anas", "Anise", "Anouar", "Ayoub", "Azziz", "Bachir", "Badr", "Baligh", "Billal", "Bouzid", "Chafik", "Chahid", "Chaker", "Chams", "Chawki", "Chokri", "Daoud", "Djamel", "Djillali", "Fadel", "Fahim", "Fares", "Farid", "Fathi", "Faudel", "Faouzi", "Fayçal", "Ferhat", "Fouad", "Gibril", "Ghani", "Habib", "Hafid", "Hakim", "Halim", "Hamid", "Hani", "Hichem", "Hilal", "Hosni", "Ismail", "Imed", "Jabar", "Kaddour", "Kader", "Kamel", "Karim", "Khaled", "Khalil", "Larbi", "Lotfi", "Mabrouk", "Mahboub", "Mahfoud", "Mahmoud", "Malik", "Marwan", "Mehdi", "Mohamed", "Moktar", "Moncef", "Mourad", "Nabil", "Nacer", "Nadir", "Nadjib", "Nassim", "Nazim", "Nouh", "Omar", "Othmane", "Usama", "Rabah", "Rachid", "Rafik", "Rahal", "Ramzi", "Raouf", "Rayan", "Réda", "Redouane", "Riad", "Rochdi", "Sabri", "Salah", "Salim", "Sami", "Sedik", "Sofiane", "Tahar", "Taimim", "Tarek", "Tayeb", "Tawfik", "Wallid", "Wassim", "Yacoub", "Yazid", "Youcef", "Younes", "Zahid", "Zaïm", "Zakaria", "Zaki", "Ziad", "Zoheir", "AbdulRahman", "Zohdy", "Gamal", "Mobarak", "Samy", "Zain", "Malek", "Saeed", "Mohaab", "Mido"];
+for (let i = 0; i < total_accounts; i++) {
+
+
+    let firstname = EN_names[Math.floor((Math.random() * EN_names.length) + 1)]; //Random first name
+    let lastname = EN_names[Math.floor((Math.random() * EN_names.length) + 1)]; //Random last name
+    let username = usrname(16); //Random letters
+    let password = usrname(9); //Random password
+    //Start Macro
+    let macro = "CODE:";
+    macro += "CLEAR" + jsLF;
+    macro += "SET !USERAGENT \"" + usragent + "\"" + jsLF; //Set Useragent
+    macro += "URL GOTO=https://accounts.google.com/SignUp" + jsLF; //Go to Gmail
+    macro += "SET !TIMEOUT_STEP 3" + jsLF; //Wait 3 seconds before step timeout
+    macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:createaccount ATTR=ID:FirstName CONTENT=" + firstname + "" + jsLF; //Set first name for gmail
+    macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:createaccount ATTR=ID:LastName CONTENT=" + lastname + "" + jsLF; //Set last name for gmail
+    macro += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:createaccount ATTR=ID:GmailAddress CONTENT=" + username + "" + jsLF; //Set username for gmail
+    macro += "SET !ENCRYPTION NO" + jsLF; //Don't encrypt password
+    macro += "TAG POS=1 TYPE=INPUT:PASSWORD FORM=ID:createaccount ATTR=ID:Passwd CONTENT=" + password + "" + jsLF; //Set password for gmail
+    macro += "TAG POS=1 TYPE=INPUT:PASSWORD FORM=ID:createaccount ATTR=ID:PasswdAgain CONTENT=" + password + "" + jsLF; //Repeat password
+    macro += "TAG POS=1 TYPE=SELECT FORM=ID:createaccount ATTR=ID:BirthMonth CONTENT=%01" + jsLF; //Set Birth Month
+    macro += "TAG POS=1 TYPE=SELECT FORM=ID:createaccount ATTR=ID:BirthDay CONTENT=%1" + jsLF; //Set Birth Day
+    macro += "TAG POS=1 TYPE=SELECT FORM=ID:createaccount ATTR=ID:BirthYear CONTENT=%1990" + jsLF; //
+    macro += "TAG POS=1 TYPE=SELECT FORM=ID:createaccount ATTR=ID:Gender CONTENT=%OTHER" + jsLF;
+    // macro += "TAG POS=5 TYPE=SELECT FORM=ID:createaccount ATTR=* CONTENT=%+20" + jsLF;
+    // macro += "TAG POS=1 TYPE=INPUT:TEL FORM=ID:createaccount ATTR=ID:RecoveryPhoneNumber CONTENT=" + myphone + "" + jsLF;
+    // macro += "TAG POS=1 TYPE=INPUT:TEL FORM=ID:createaccount ATTR=ID:RecoveryPhoneNumber CONTENT="+randomnumber+"" + jsLF;
+    macro += "WAIT SECONDS=3" + jsLF; //Wait 3 seconds before submit
+    macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:createaccount ATTR=ID:submitbutton" + jsLF; //Submit everything
+    macro += "TAG POS=1 TYPE=DIV ATTR=ID:tos-scroll-button" + jsLF; //Scroll terms
+    macro += "TAG POS=1 TYPE=INPUT:BUTTON ATTR=ID:iagreebutton" + jsLF; //Agree on terms
+    macro += "SET !ERRORIGNORE YES" + jsLF; //Ignore anything else
+    macro += "SET !TIMEOUT_STEP 3" + jsLF; //3 Seconds for step timeout.
+    macro += "TAG POS=1 TYPE=INPUT:SUBMIT FORM=ID:signupidv ATTR=ID:next-button&&NAME:SendCode&&VALUE:Continue&&CLASS:g-button<SP>g-button-submit&&TYPE:submit" + jsLF; //Submit everything
+    iimDisplay("Current Account: " + i + "\n");
+    iimPlay(macro);
+    //Extract Account Details
+    let result = iimGetLastExtract(1);
+    let result2 = iimGetLastExtract(2);
+    if (result === "#EANF#" && result2 === "#EANF#") {
+        //Save username,password at GmailAccounts.csv
+        iimPlayCode("SET !EXTRACT " + username + "@gmail.com\nADD !EXTRACT " + password + "\nSAVEAS TYPE=EXTRACT FOLDER=* FILE=GmailAccounts.csv");
+    } else {
+        //Save username,password at GmailAccounts.csv then add seurity check phone number!
+        iimPlayCode("SET !EXTRACT " + username + "@gmail.com\nADD !EXTRACT " + password + "\nADD !EXTRACT \"" + trouble + "\"\nSAVEAS TYPE=EXTRACT FOLDER=* FILE=GmailAccounts.csv");
+    }
+}
